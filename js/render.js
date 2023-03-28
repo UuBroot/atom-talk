c65320286345df55a4264966b2481e0b377dd8ff
UuBroot <lolylyt@gmail.com>
2023-03-28 21:12:45 +0200

    first design steps for side bar

---
+++ js/render.js
@@ -0,0 +1,11 @@
+let roomSelection = document.getElementById("main-roomSelection");
+
+
+
+
+//Testing
+for(let i = 0;i<100;i++){
+    roomSelection.innerHTML += `
+        <div id="server-icon" onclick=""></div>
+    `
+}
--- main.js
+++ main.js
@@ -2,22 +2,35 @@
  *  imports and requires    *
  **************************/
 const { app, BrowserWindow } = require('electron')
+const { join } = require("path");
+/**************
+ *  Variables   *
+ **************/
 
 /******************
  *  Electron Stuff  *
  ******************/
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-    win.loadFile('./app/index.html')
-}
 //Onstarte
 app.whenReady().then(() => {
-    createWindow()
+    main();
 })
 //Onclose
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
+/**********
+ *  Main    *
+ **********/
+function main() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false,
+        autoHideMenuBar: true
+      })
+    win.loadFile('./app/index.html');
+    win.on("ready-to-show", win.show); 
+    win.on("resized", console.log(win.getSize))
+}
+
+
