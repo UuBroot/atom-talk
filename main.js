d2bcfe16aa32312cf4abbdeb706d0a2050f81b6a
UuBroot <lolylyt@gmail.com>
2023-04-07 09:15:20 +0200

    idk

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  *  imports and requires    *
  **************************/
 const { app, BrowserWindow } = require('electron')
-const { join } = require("path");
+
 /**************
  *  Variables   *
  **************/
@@ -10,27 +10,41 @@ const { join } = require("path");
 /******************
  *  Electron Stuff  *
  ******************/
+
+async function startClient(){
+    //await client.startClient({initialSyncLimit: 10});
+    //client.once('sync', function(state, prevState, res) {
+        console.log("myass")
+        //if(state === 'PREPARED') {
+            const win = new BrowserWindow({
+                width: 800,
+                height: 600,
+                show: false,
+                autoHideMenuBar: true
+              })
+            win.loadFile('./app/index.html');
+            win.on("resized", () => {
+                console.log("AAAAAAAAAAA")
+            })
+            win.on("ready-to-show", win.show); 
+       // } else {
+       //     console.log(state);
+       //     process.exit(1);
+        //}
+
+
+    //});    
+
+}
+
+
+
 //Onstarte
 app.whenReady().then(() => {
-    main();
+    startClient();
 })
 //Onclose
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
-/**********
- *  Main    *
- **********/
-function main() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        show: false,
-        autoHideMenuBar: true
-      })
-    win.loadFile('./app/index.html');
-    win.on("ready-to-show", win.show); 
-    win.on("resized", console.log(win.getSize))
-}
-
 
